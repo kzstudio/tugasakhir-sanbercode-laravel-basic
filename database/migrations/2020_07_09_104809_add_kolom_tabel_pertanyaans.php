624b21b0e3b5a49a47f9b1be6cea88313a279d6b
M Iqbal laksana <koori.no.kiretsu@gmail.com>
2020-07-09 18:32:20 +0700

    update tabel

---
+++ database/migrations/2020_07_09_104809_add_kolom_tabel_pertanyaans.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddKolomTabelPertanyaans extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pertanyaans', function (Blueprint $table) {
+             $table->text('tags')->nullable();
+             $table->integer('jumlah_upvote')->default(0);
+             $table->integer('jumlah_downvote')->default(0);
+             $table->integer('total_poinvote')->defualt(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pertanyaans', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2020_07_09_105518_add_kolom_tabel_jawabans.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddKolomTabelJawabans extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('jawabans', function (Blueprint $table) {
+            $table->integer('jumlah_upvote')->default(0);
+            $table->integer('jumlah_downvote')->default(0);
+            $table->integer('total_poinvote')->defualt(0);
+            $table->boolean('resolved')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jawabans', function (Blueprint $table) {
+            //
+        });
+    }
+}
