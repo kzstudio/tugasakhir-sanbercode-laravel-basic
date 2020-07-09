dec5f7edc729f17bc6a5565ae905edcd7e78672f
argadhana <yoshspecta333@gmail.com>
2020-07-09 13:39:24 +0700

    Baru sampe Create Pertanyaan

---
+++ database/migrations/2020_07_09_045438_create_kepuasans_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKepuasansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kepuasans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->boolean('like');
+            $table->boolean('dislike');
+            $table->unsignedBigInteger('pertanyaan_id');
+            $table->unsignedBigInteger('jawaban_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('pertanyaan_id')->references('id')->on('pertanyaans');
+            $table->foreign('jawaban_id')->references('id')->on('jawabans');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kepuasans');
+    }
+}
---
+++ resources/views/layouts/layouting.blade.php
@@ -0,0 +1,6 @@
+@include('layouts.head')
+@include('layouts.navbar')
+@include('layouts.sidebar')
+@include('layouts.wrapper')
+@yield('content')
+@include('layouts.footer')
