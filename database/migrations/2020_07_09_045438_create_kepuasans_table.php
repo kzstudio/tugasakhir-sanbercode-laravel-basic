f307af71a51376f8a0694196f0c4b04375fb549b
M Iqbal laksana <koori.no.kiretsu@gmail.com>
2020-07-09 17:25:15 +0700

    perbaikan conflict

--- database/migrations/2020_07_09_045438_create_kepuasans_table.php
+++ database/migrations/2020_07_09_045438_create_kepuasans_table.php
@@ -20,11 +20,6 @@ class CreateKepuasansTable extends Migration
             $table->unsignedBigInteger('pertanyaan_id');
             $table->unsignedBigInteger('jawaban_id');
             $table->unsignedBigInteger('user_id');
-<<<<<<< HEAD
-
-=======
-                //teskoneksi
->>>>>>> 52252f99cfb3a517e994066461bd47fb7c8ccb18
             $table->foreign('pertanyaan_id')->references('id')->on('pertanyaans');
             $table->foreign('jawaban_id')->references('id')->on('jawabans');
             $table->foreign('user_id')->references('id')->on('users');
